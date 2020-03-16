bdbec65e035d9001af54e8118bd3e680c008c024
Admin <adsuarajunneilc001@gmail.com>
2020-03-17 07:26:35 +0800

    gfood rev

--- app/src/main/java/edu/ucucite/gfood/CategoriesActivity.java
+++ app/src/main/java/edu/ucucite/gfood/CategoriesActivity.java
@@ -11,4 +11,6 @@ public class CategoriesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
     }
+
+
 }
--- app/src/main/java/edu/ucucite/gfood/MainActivity.java
+++ app/src/main/java/edu/ucucite/gfood/MainActivity.java
@@ -2,7 +2,9 @@ package edu.ucucite.gfood;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void onclkstart(View view) {
+        Intent intent = new Intent(MainActivity.this, CategoriesActivity.class);
+        startActivity(intent);
     }
 }
